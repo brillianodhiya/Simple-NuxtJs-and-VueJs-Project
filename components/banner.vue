72b710f684a486f9b02fa4f5743c23b820745bd6
brillianodhiya <brillidhiya@gmail.com>
2019-10-06 23:52:28 +0700

    add template layout

---
+++ components/banner.vue
@@ -0,0 +1,59 @@
+<template>
+    <div>
+        <div v-swiper:mySwiper="swiperOption">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide" v-for="item in banner" v-bind:key="item.key">
+                    <img :src="item.src" alt="item.src">
+                </div>
+            </div>
+            <div class="swiper-pagination" slot="pagination"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            banner: [{
+                src: 'https://dummyimage.com/800x400/206de0/ffffff'
+            }, {
+                src: 'https://dummyimage.com/800x400/206de0/ffffff'
+            }, {
+                src: 'https://dummyimage.com/800x400/206de0/ffffff'
+            }, {
+                src: 'https://dummyimage.com/800x400/206de0/ffffff'
+            }, {
+                src: 'https://dummyimage.com/800x400/206de0/ffffff'
+            }, {
+                src: 'https://dummyimage.com/800x400/206de0/ffffff'
+            }],
+            swiperOption: {
+                slidesPerView: 1,
+                spaceBetween: 8,
+                breakpoints: {
+                    320: {
+                        slidesPerView: 1,
+                        spaceBetween: 8
+                    },
+                    400: {
+                        slidesPerView: 2,
+                        spaceBetween: 8
+                    },
+                    800: {
+                        slidesPerView: 3,
+                        spaceBetween: 8
+                    }
+                },
+                autoplay: {
+                    delay: 5000,
+                    disableOnInteraction: false
+                },
+                pagination: {
+                    el: '.swiper-pagination'
+                }
+            }
+        }
+    }
+}
+</script>
---
+++ pages/detail.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="mt-5">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">Post Title</h5>
+                        <h6>Sunday, 06 October 2019</h6>
+                        <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste inventore veritatis illo pariatur, tempore quis ullam dolorum ea, omnis aperiam quo laborum fugiat dicta! Dignissimos corrupti sint maxime facere dolores.</p>
+                        <a href="#" class="card-link">Card Link</a>
+                        <a href="#" class="card-link">Another Link</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    layout: 'blog'
+}
+</script>
