10fd72a15cdb79402fbecdf8e405dc1a04270088
brillianodhiya <brillidhiya@gmail.com>
2019-10-07 21:30:15 +0700

    add vuex and integration with API

--- pages/detail.vue
+++ pages/detail.vue
@@ -1,15 +1,12 @@
 <template>
     <div class="mt-5">
-        <div class="row">
-            <div class="col-12">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Post Title</h5>
-                        <h6>Sunday, 06 October 2019</h6>
-                        <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste inventore veritatis illo pariatur, tempore quis ullam dolorum ea, omnis aperiam quo laborum fugiat dicta! Dignissimos corrupti sint maxime facere dolores.</p>
-                        <a href="#" class="card-link">Card Link</a>
-                        <a href="#" class="card-link">Another Link</a>
-                    </div>
+        <div class="container">
+            <div class="row justify-content-md-center">
+                <div class="col-8">
+                    <img class="img-fluid" :src="article.urlToImage" alt="Card image cap">
+                    <p class="card-text"><small class="text-muted">{{ article.publishedAt }} - {{ article.author }} - {{ article.source.name }}</small></p>
+                    <h5 class="card-title">{{ article.title }}</h5>
+                    <p class="card-text">{{ article.description }}</p>
                 </div>
             </div>
         </div>
@@ -18,6 +15,10 @@
 
 <script>
 export default {
-    layout: 'blog'
+    computed: {
+        article () {
+            return this.$store.state.article
+        }
+    }
 }
 </script>
